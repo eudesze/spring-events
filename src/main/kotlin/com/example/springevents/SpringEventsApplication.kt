fad9483c6245d3fe5b58a9590c25fc24a50d5f06
eudes <zeeudes.souza@gmail.com>
2022-11-26 22:53:56 -0300

    feat: add new project

---
+++ src/main/kotlin/com/example/springevents/SpringEventsApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springevents
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringEventsApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringEventsApplication>(*args)
+}
